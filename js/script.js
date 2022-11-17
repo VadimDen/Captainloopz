a921c8be4cb71e723edbd6cf5ca39e74ef458f13
Vadim Denisov <85564608+VadimDen@users.noreply.github.com>
2022-11-18 01:20:32 +0300

    Новые страницы. Анимации переходов.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict'
 
+
 //menu button
 const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
@@ -20,4 +21,16 @@ textrev.from(".line span", 1, {
     stagger: {
         amount: 0.4
     }
+});
+
+//main page button location change animation
+const exploreLink = document.querySelector('#explore');
+const swupElem = document.querySelector('#swup');
+const body = document.querySelector('body');
+
+
+console.log(swupElem);
+exploreLink.addEventListener('click', ()=> {
+    swupElem.style = "opacity: 0; transition: 500ms;";
+    location.href = "about.html";
 });
