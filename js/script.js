42461e231e48bc1b9c2de47cd93f03759b40dbb9
Vadim Denisov <85564608+VadimDen@users.noreply.github.com>
2023-01-03 21:54:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@ linksBD.forEach(elem => {
 function getCurrentURL() {
     return window.location.pathname
 }
-if (getCurrentURL() == '/Captainloopz/project.html') {
+if (getCurrentURL() == '/project.html' || getCurrentURL() == '/Captainloopz/project.html') {
     const projectsBD = {
         P1: {
             name: 'Vaporwave Bodies Vj Pack',
@@ -70,10 +70,10 @@ if (getCurrentURL() == '/Captainloopz/project.html') {
             contentType: 'Моушн-Дизайн',
             videoURL: 'https://previews.customer.envatousercontent.com/files/9c016c4b-8517-4e06-9a76-f59c5f2cdeb2/video_preview_h264.mp4',
             picturesSRC: {
-                first: 'img/projects/001/first.jpg',
-                second: 'img/projects/001/second.jpg',
-                third: 'img/projects/001/third.jpg',
-                forth: 'img/projects/001/forth.jpg',
+                first: 'img/projects/P001/first.jpg',
+                second: 'img/projects/P001/second.jpg',
+                third: 'img/projects/P001/third.jpg',
+                forth: 'img/projects/P001/forth.jpg',
                 video: 'img/projects/Vaporwave_Bodies_Vj_Pack.avif'
             }
         },
@@ -85,10 +85,10 @@ if (getCurrentURL() == '/Captainloopz/project.html') {
             contentType: 'Моушн-Дизайн',
             videoURL: 'https://previews.customer.envatousercontent.com/files/40862121-9c22-4a9e-9b5e-78f1628cbb89/video_preview_h264.mp4',
             picturesSRC: {
-                first: 'img/projects/002/first.jpg',
-                second: 'img/projects/002/second.jpg',
-                third: 'img/projects/002/third.jpg',
-                forth: 'img/projects/002/forth.jpg',
+                first: 'img/projects/P002/first.jpg',
+                second: 'img/projects/P002/second.jpg',
+                third: 'img/projects/P002/third.jpg',
+                forth: 'img/projects/P002/forth.jpg',
                 video: 'img/projects/Low_Poly_Animals.avif'
             }
         },
@@ -100,10 +100,10 @@ if (getCurrentURL() == '/Captainloopz/project.html') {
             contentType: 'Моушн-Дизайн',
             videoURL: 'https://previews.customer.envatousercontent.com/files/97621d05-c787-4f40-a516-71af2f836ea2/video_preview_h264.mp4',
             picturesSRC: {
-                first: 'img/projects/003/first.jpg',
-                second: 'img/projects/003/second.jpg',
-                third: 'img/projects/003/third.jpg',
-                forth: 'img/projects/003/forth.jpg',
+                first: 'img/projects/P003/first.jpg',
+                second: 'img/projects/P003/second.jpg',
+                third: 'img/projects/P003/third.jpg',
+                forth: 'img/projects/P003/forth.jpg',
                 video: 'img/projects/Body_Parts_Vaporwave.avif'
             }
         },
@@ -115,10 +115,10 @@ if (getCurrentURL() == '/Captainloopz/project.html') {
             contentType: 'Моушн-Дизайн',
             videoURL: 'https://previews.customer.envatousercontent.com/files/c5a51969-1ec4-4d5f-bb0b-fdb77cf2a0e9/video_preview_h264.mp4',
             picturesSRC: {
-                first: 'img/projects/004/first.jpg',
-                second: 'img/projects/004/second.jpg',
-                third: 'img/projects/004/third.jpg',
-                forth: 'img/projects/004/forth.jpg',
+                first: 'img/projects/P004/first.jpg',
+                second: 'img/projects/P004/second.jpg',
+                third: 'img/projects/P004/third.jpg',
+                forth: 'img/projects/P004/forth.jpg',
                 video: 'img/projects/Ancient_Power_V_2.avif'
             }
         },
@@ -130,10 +130,10 @@ if (getCurrentURL() == '/Captainloopz/project.html') {
             contentType: 'Моушн-Дизайн',
             videoURL: 'https://previews.customer.envatousercontent.com/files/00220fc4-d3d4-459b-9827-4b0972680a23/video_preview_h264.mp4',
             picturesSRC: {
-                first: 'img/projects/005/first.jpg',
-                second: 'img/projects/005/second.jpg',
-                third: 'img/projects/005/third.jpg',
-                forth: 'img/projects/005/forth.jpg',
+                first: 'img/projects/P005/first.jpg',
+                second: 'img/projects/P005/second.jpg',
+                third: 'img/projects/P005/third.jpg',
+                forth: 'img/projects/P005/forth.jpg',
                 video: 'img/projects/Neon_Hoops_Vj_Pack.avif'
             }
         },
@@ -145,10 +145,10 @@ if (getCurrentURL() == '/Captainloopz/project.html') {
             contentType: 'Моушн-Дизайн',
             videoURL: 'https://previews.customer.envatousercontent.com/files/d74a2ac8-8234-4759-b520-0c449c600ae9/video_preview_h264.mp4',
             picturesSRC: {
-                first: 'img/projects/006/first.jpg',
-                second: 'img/projects/006/second.jpg',
-                third: 'img/projects/006/third.jpg',
-                forth: 'img/projects/006/forth.jpg',
+                first: 'img/projects/P006/first.jpg',
+                second: 'img/projects/P006/second.jpg',
+                third: 'img/projects/P006/third.jpg',
+                forth: 'img/projects/P006/forth.jpg',
                 video: 'img/projects/Heads_&_Bodies_in_Vaporwave.avif'
             }
         }
